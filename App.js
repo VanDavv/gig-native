89a634a1d2cb4b11007d5a0a2c2b8747d8391c1b
Łukasz Piłatowski <lpilatowski@teonite.com>
2018-05-06 18:59:56 +0200

    add dynamic mock markers

--- App.js
+++ App.js
@@ -1,38 +1,57 @@
 import React from 'react';
-import MapView from 'react-native-maps';
-import { StyleSheet, View } from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
+import {StyleSheet, View} from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    height: '100%',
-    width: '100%',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
+    container: {
+        ...StyleSheet.absoluteFillObject,
+        height: '100%',
+        width: '100%',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject,
+    },
 });
 
 export default class MyApp extends React.Component {
-  render() {
-    const { region } = this.props;
-    console.log(region);
+    constructor(props) {
+        super(props);
+        this.state = {
+            region: {
+                latitude: 37.78825,
+                longitude: -122.4324,
+                latitudeDelta: 0.015,
+                longitudeDelta: 0.0121,
+            },
+            markers: []
+        }
+    }
 
-    return (
-      <View style ={styles.container}>
-        <MapView
-          style={styles.map}
-          region={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.0121,
-          }}
-        >
-        </MapView>
-      </View>
-    );
-  }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <MapView
+                    initialRegion={this.state.region}
+                    style={styles.map}
+                    onPress={event => this.setState({markers: this.state.markers.concat({
+                            coordinate: event.nativeEvent.coordinate
+                    })})}
+                >
+                    {
+                        this.state.markers.map((marker, key) => (
+                            <Marker
+                                key={key}
+                                coordinate={marker.coordinate}
+                                title={`TEST${key}`}
+                                description={'some description'}
+                            />
+                        ))
+                    }
+                </MapView>
+            </View>
+        );
+    }
 }
